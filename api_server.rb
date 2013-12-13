35f76b0e25a0b8b80c74bcdce53180b32182bbec
Juan M. Cuello <juanmacuello@gmail.com>
2013-12-13 10:00:37 -0300

    Add routes to api server.

--- api_server.rb
+++ api_server.rb
@@ -2,7 +2,31 @@ require 'cuba'
 require './subscriptions'
 
 ApiServer = Cuba.new do
-  on get do
-    Subscriptions.notify 'channel-1', 'event', { test: 'data' }
-  end
+
+  on "apps/:app_id" do |app_id|
+
+    authenticate(req)
+
+    # Events
+    on post, "events" do
+      channels = req["channels"] || [req["channel"]]
+      Subscriptions.notify channels, req["name"], req["data"], socket_id: req["socket_id"]
+    end
+
+    # Channels
+    on get, "channels" do
+    end
+
+    on get, "channels/:channel" do
+    end
+
+    # Users
+    on get, "channels/:channel/users" do
+    end
+ end
+
+end
+
+def authenticate(req)
+  req["name"] == "test" || halt([401, {}, ['401 Unauthorized']])
 end
