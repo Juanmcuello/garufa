2a69b325b17456a23c8f4176ee8573f0b76ccf1a
Juan M. Cuello <juanmacuello@gmail.com>
2015-07-14 19:54:10 -0300

    Move api routes to modules

--- lib/garufa/api/routes/channels.rb
+++ lib/garufa/api/routes/channels.rb
@@ -1,33 +1,37 @@
 module Garufa
   module API
-    Server.route('channels') do |r|
-      r.on 'channels' do
+    module Routes
+      module Channels
+        Server.route('channels') do |r|
+          r.on 'channels' do
 
-        stats = Stats.new(Subscriptions.all)
+            stats = Stats.new(Subscriptions.all)
 
-        # GET channels
-        r.is do
-          render 'channels', locals: { stats: stats.all_channels, filter: filter(request.params) }
-        end
+            # GET channels
+            r.is do
+              render 'channels', locals: { stats: stats.all_channels, filter: filter(request.params) }
+            end
 
-        # GET channels/presence-channel
-        r.is "presence-:channel" do |channel|
-          render 'presence', locals: { stats: stats.single_channel(channel), filter: filter(request.params) }
-        end
+            # GET channels/presence-channel
+            r.is "presence-:channel" do |channel|
+              render 'presence', locals: { stats: stats.single_channel(channel), filter: filter(request.params) }
+            end
 
-        # GET channels/presence-channel/users
-        r.is /(presence-.+)\/users/ do |channel|
-          render 'presence_users', locals: { stats: stats.single_channel(channel) }
-        end
+            # GET channels/presence-channel/users
+            r.is /(presence-.+)\/users/ do |channel|
+              render 'presence_users', locals: { stats: stats.single_channel(channel) }
+            end
 
-        # GET channels/non-presence-channel
-        r.is ":channel" do |channel|
-          render 'non_presence', locals: { stats: stats.single_channel(channel) }
-        end
+            # GET channels/non-presence-channel
+            r.is ":channel" do |channel|
+              render 'non_presence', locals: { stats: stats.single_channel(channel) }
+            end
 
-        # GET channels/non-presence-channel/users
-        r.is ":channel/users" do |channel|
-          response.status = 400
+            # GET channels/non-presence-channel/users
+            r.is ":channel/users" do |channel|
+              response.status = 400
+            end
+          end
         end
       end
     end
--- lib/garufa/api/routes/events.rb
+++ lib/garufa/api/routes/events.rb
@@ -1,19 +1,23 @@
 module Garufa
   module API
-    Server.route('events') do |r|
-      r.is 'events' do
-        handler = EventHandler.new
-        handler.handle(read_body)
-        response.status = 202
-        '{}'
-      end
+    module Routes
+      module Events
+        Server.route('events') do |r|
+          r.is 'events' do
+            handler = EventHandler.new
+            handler.handle(read_body)
+            response.status = 202
+            '{}'
+          end
 
-      # Legacy events
-      r.is 'channels/:channel/events' do |channel|
-        handler = EventHandler.new
-        handler.handle_legacy(read_body, channel, request.GET)
-        response.status = 202
-        '{}'
+          # Legacy events
+          r.is 'channels/:channel/events' do |channel|
+            handler = EventHandler.new
+            handler.handle_legacy(read_body, channel, request.GET)
+            response.status = 202
+            '{}'
+          end
+        end
       end
     end
   end
